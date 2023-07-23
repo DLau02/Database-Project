8b8d628d17baf4383275661175bf5707f085df79
Yuya216 <139820362+Yuya216@users.noreply.github.com>
2023-07-23 17:31:15 -0500

    Update Insurance_Pay_For.sql

--- Insurance_Pay_For.sql
+++ Insurance_Pay_For.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Insurance_Pay_For (
-    invoice_ID VARCHAR(8),
+    invoice_ID VARCHAR(50),
     Provider VARCHAR(20),
     FOREIGN KEY (invoice_ID) REFERENCES Invoice(invoice_ID),
     FOREIGN KEY (Provider) REFERENCES Insurance(Provider)
