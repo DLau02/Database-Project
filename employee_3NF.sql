9d1f695e582fb8f635390f8d5cd6ea51810ba96f
DLau02 <106625128+DLau02@users.noreply.github.com>
2023-07-23 15:30:08 -0500

    Create employee_3NF.sql

---
+++ employee_3NF.sql
@@ -0,0 +1,20 @@
+CREATE TABLE employee (
+  SSN INT(9) NOT NULL,
+  Specialty VARCHAR(50) NOT NULL UNIQUE,
+  Super_SSN INT(9) NOT NULL UNIQUE,
+  PRIMARY KEY (SSN)
+);
+
+Create Table Specialty_Salary (
+	Specialty VARCHAR(50) NOT NULL,
+    Salary int(10) NOT NULL,
+    PRIMARY KEY (Specialty),
+    Foreign Key (Specialty) REFERENCES employee(Specialty)
+);
+
+Create Table Department_Supervisor (
+	Super_SSN INT(9) NOT NULL,
+    Department varchar(50) NOT NULL,
+    PRIMARY KEY (Super_SSN),
+    Foreign Key (Super_SSN) REFERENCES employee(Super_SSN)
+);
