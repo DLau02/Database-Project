ddd263570d01f35ffae0d91ef13d8582df400b80
DLau02 <106625128+DLau02@users.noreply.github.com>
2023-07-23 16:21:44 -0500

    Create Procedure_Contains.sql

---
+++ Procedure_Contains.sql
@@ -0,0 +1,7 @@
+create table Procedure_Contains (
+	PID varchar(50) Not Null Unique,
+    Invoice_ID varchar(50) Not Null,
+    Primary Key (Invoice_ID),
+    foreign key (PID) references Procedure_Info(PID),
+    foreign key (Invoice_ID) references Invoice(Invoice_ID)
+);
