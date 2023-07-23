f48684ce05e31c0b20443d199fda1c01b141a702
DLau02 <106625128+DLau02@users.noreply.github.com>
2023-07-23 16:01:20 -0500

    Create Patient.sql

---
+++ Patient.sql
@@ -0,0 +1,7 @@
+Create Table Patient(
+	  SSN int(9) Not Null,
+    Room_Num Int(5) Not Null,
+    primary key (SSN),
+    foreign key (SSN) references person(SSN),
+    foreign key (Room_Num) references room(Room_Num)
+);
