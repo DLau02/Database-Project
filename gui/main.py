fea07d78ad7d4d309e9a0b032b533f4c1f2deba3
Kayla Baker <kaylambaker456@gmail.com>
2023-07-30 17:41:44 -0500

    finish PatientOpts.py and update main with frames

--- gui/main.py
+++ gui/main.py
@@ -1,10 +1,14 @@
 import sys
 import customtkinter as ctk
-import mysql.connector
-from PatientOpts import PatientByName, PatientOpts, AddPatient
-from SupplyOpts import SupplyOpts, AddMedication, SearchMedication, AddEquipment, SearchEquipment, SuppliesforProcedure
-
-from dbConfig import db
+from PatientOpts import *
+from SupplyOpts import (
+    SupplyOpts,
+    AddMedication,
+    SearchMedication,
+    AddEquipment,
+    SearchEquipment,
+    SuppliesforProcedure,
+)
 
 ctk.set_appearance_mode("system")
 ctk.set_default_color_theme("dark-blue")
@@ -14,30 +18,34 @@ class App(ctk.CTk):
     def __init__(self, *args, **kwargs):
         ctk.CTk.__init__(self, *args, **kwargs)
         self.geometry("800x500")
-
-        # the container is where we'll stack a bunch of frames
-        # on top of each other, then the one we want visible
-        # will be raised above the others
         container = ctk.CTkFrame(self)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        for F in (StartPage, PatientOpts, AddPatient, PatientByName, SupplyOpts, AddMedication, SearchMedication, AddEquipment, SearchEquipment,  SuppliesforProcedure):
+        for F in (
+            StartPage,
+            PatientOpts,
+            AddPatient,
+            PatientByName,
+            PatientBySSN,
+            ChangePatient,
+            SupplyOpts,
+            AddMedication,
+            SearchMedication,
+            AddEquipment,
+            SearchEquipment,
+            SuppliesforProcedure,
+        ):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
-
-            # put all of the pages in the same location;
-            # the one on the top of the stacking order
-            # will be the one that is visible.
             frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame("StartPage")
 
     def show_frame(self, page_name):
-        """Show a frame for the given page name"""
         frame = self.frames[page_name]
         frame.tkraise()
 
@@ -55,7 +63,7 @@ class StartPage(ctk.CTkFrame):
         )
         SupplyOpts = ctk.CTkButton(
             self,
-            text="Supplies", 
+            text="Supplies",
             command=lambda: controller.show_frame("SupplyOpts"),
         )
         exit_btn = ctk.CTkButton(self, text="Exit", command=lambda: sys.exit(0))
@@ -64,26 +72,6 @@ class StartPage(ctk.CTkFrame):
         patient_by_name.pack()
         SupplyOpts.pack()
         exit_btn.pack()
-
-
-class DoctorByName(ctk.CTkFrame):
-    pass
-
-
-class ChangePatientRoom(ctk.CTkFrame):
-    pass
-
-
-class SearchSupply(ctk.CTkFrame):
-    pass
-
-
-class SearchSupplyByName(ctk.CTkFrame):
-    pass
-
-
-class SearchSupplyByNumber(ctk.CTkFrame):
-    pass
 
 
 if __name__ == "__main__":
