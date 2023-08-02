76277bef23a08445c7d480e94edec8c6d968cd24
Kayla Baker <kaylambaker456@gmail.com>
2023-08-02 13:19:38 -0500

    Patient opts with prepared statement

--- gui/PatientOpts.py
+++ gui/PatientOpts.py
@@ -2,9 +2,23 @@ import customtkinter as ctk
 from dbConfig import db
 
 
+def getPatients() -> list[str]:
+    query = ( """select ssn,first_name,last_name from patient as pa natural join person as pe;""")
+    mycursor = db.cursor()
+    mycursor.execute(query)
+    result = mycursor.fetchall()
+    patients = [
+        str(patient[0]) + " " + str(patient[1]) + " " + str(patient[2])
+        for patient in result
+    ]
+    if len(patients) == 0:
+        return [""]
+    return patients
+
+
 def getUnoccupiedPatientRooms() -> list[str]:
     # get unoccupied patient room numbers
-    query = "select room_num from room where room_type='Patient Room' and room_num not in (select room_num from patient);"
+    query = """select room_num from room where room_type='Patient Room' and room_num not in (select room_num from patient);"""
     mycursor = db.cursor()
     mycursor.execute(query)
     unoccupied_rooms = mycursor.fetchall()
@@ -87,18 +101,19 @@ class PatientByName(ctk.CTkFrame):
         super().tkraise()
 
     def search(self):
-        query = "select first_name,last_name,pa.ssn,address,birthdate,room_num from patient as pa join person as pe on pa.ssn=pe.ssn where first_name='{}' and last_name='{}' ".format(
-            self.first.get(), self.last.get()
-        )
+        query = """select * from patient natural join person where first_name=%s and last_name=%s;"""
+        # query = "select first_name,last_name,pa.ssn,address,birthdate,room_num from patient as pa join person as pe on pa.ssn=pe.ssn where first_name='{}' and last_name='{}' ".format(
+        #     self.first.get(), self.last.get()
+        # )
         mycursor = db.cursor()
-        mycursor.execute(query)
+        mycursor.execute(query, (self.first.get(), self.last.get()))
         people = mycursor.fetchall()
         labelText = "There are {} patients named {} {}\n".format(
             len(people), self.first.get(), self.last.get()
         )
         for person in people:
             labelText += "Name: {} {}\nSSN: {}\nBirthdate: {}\nAddress: {}\nRoom Number:{}\n\n".format(
-                person[0], person[1], person[2], person[3], person[4], person[5]
+                person[4], person[3], person[0], person[5], person[2], person[1]
             )
         self.resultsBox.configure(state=ctk.NORMAL)
         self.resultsBox.delete("0.0", "end")
@@ -164,20 +179,24 @@ class AddPatient(ctk.CTkFrame):
         super().tkraise()
 
     def add(self):
-        person_query = "insert into person values ({}, '{}', '{}', '{}', STR_TO_DATE('{}','%Y-%m-%d'));"
-        patient_query = "insert into patient values ({}, {})"
+        person_query = """insert into person values (%s, %s, %s, %s, STR_TO_DATE(%s,'%Y-%m-%d'));"""
+        patient_query = """insert into patient values (%s, %s)"""
         mycursor = db.cursor()
         try:
             mycursor.execute(
-                person_query.format(
+                person_query,
+                (
                     self.ssn.get(),
                     self.address.get(),
                     self.last.get(),
                     self.first.get(),
                     self.birthdate.get(),
-                )
+                ),
             )
-            mycursor.execute(patient_query.format(self.ssn.get(), self.roomNum.get()))
+            mycursor.execute(
+                patient_query, (int(self.ssn.get()), int(self.roomNum.get()))
+            )
+            mycursor.fetchall()
             db.commit()
             self.resultsBox.configure(state=ctk.NORMAL)
             self.resultsBox.delete("0.0", "end")
@@ -190,7 +209,8 @@ class AddPatient(ctk.CTkFrame):
                 values=getUnoccupiedPatientRooms(),
             )
             self.roomNum.set(self.roomNum._values[0])
-        except:
+        except Exception as e:
+            print(e)
             self.resultsBox.configure(state=ctk.NORMAL)
             self.resultsBox.delete("0.0", "end")
             self.resultsBox.insert(
@@ -211,16 +231,18 @@ class ChangePatient(ctk.CTkFrame):
         self.label = ctk.CTkLabel(
             self, text="Select the patient you would like to change"
         )
-        self.patients = ctk.CTkOptionMenu(self, values=self.getPatients())
+        self.patients = ctk.CTkOptionMenu(self, values=getPatients(), width=200)
         self.getPatient = ctk.CTkButton(
             self, text="Get patient", command=self.populateEntries
         )
-        self.first = ctk.CTkEntry(self, placeholder_text="First name")
-        self.last = ctk.CTkEntry(self, placeholder_text="Last name")
+        self.first = ctk.CTkEntry(self, placeholder_text="First name", width=200)
+        self.last = ctk.CTkEntry(self, placeholder_text="Last name", width=200)
         self.roomNumLabel = ctk.CTkLabel(self, text="Room number")
         self.roomNum = ctk.CTkOptionMenu(self, values=getUnoccupiedPatientRooms())
-        self.birthdate = ctk.CTkEntry(self, placeholder_text="Birthdate: YYYY-MM-DD")
-        self.address = ctk.CTkEntry(self, placeholder_text="Address")
+        self.birthdate = ctk.CTkEntry(
+            self, placeholder_text="Birthdate: YYYY-MM-DD", width=200
+        )
+        self.address = ctk.CTkEntry(self, placeholder_text="Address", width=200)
         self.changeBtn = ctk.CTkButton(self, text="Change", command=self.change)
 
         self.back = ctk.CTkButton(
@@ -245,19 +267,23 @@ class ChangePatient(ctk.CTkFrame):
 
     def change(self):
         try:
-            personQuery = "update person set first_name='{}', last_name='{}', address='{}', birthdate=STR_TO_DATE('{}','%Y-%m-%d') where ssn={};".format(
-                self.first.get(),
-                self.last.get(),
-                self.address.get(),
-                self.birthdate.get(),
-                self.patients.get().split(" ")[0],  # ssn
-            )
-            patientQuery = "update patient set room_num={} where ssn={}".format(
-                self.roomNum.get(), self.patients.get().split(" ")[0]
-            )
+            personQuery = """update person set first_name=%s, last_name=%s, address=%s, birthdate=STR_TO_DATE(%s,'%Y-%m-%d') where ssn=%s;"""
+            patientQuery = """update patient set room_num=%s where ssn=%s"""
             mycursor = db.cursor()
-            mycursor.execute(personQuery)
-            mycursor.execute(patientQuery)
+            mycursor.execute(
+                personQuery,
+                (
+                    self.first.get(),
+                    self.last.get(),
+                    self.address.get(),
+                    self.birthdate.get(),
+                    self.patients.get().split(" ")[0],  # ssn
+                ),
+            )
+            mycursor.execute(
+                patientQuery, (self.roomNum.get(), self.patients.get().split(" ")[0])
+            )
+            mycursor.fetchall()
             db.commit()
             labelText = "{} information was changed".format(self.patients.get())
             self.resultsBox.configure(state=ctk.NORMAL)
@@ -265,7 +291,7 @@ class ChangePatient(ctk.CTkFrame):
             self.resultsBox.insert("0.0", labelText)
             self.resultsBox.configure(state=ctk.DISABLED)
         except Exception as e:
-            # print(e)
+            print(e)
             labelText = "Unable to change patient {}".format(self.patients.get())
             self.resultsBox.configure(state=ctk.NORMAL)
             self.resultsBox.delete("0.0", "end")
@@ -274,40 +300,25 @@ class ChangePatient(ctk.CTkFrame):
 
     def populateEntries(self):
         patientSsn = self.patients.get().split(" ")[0]
-        query = "select first_name, last_name,address,birthdate,room_num from patient as pa join person as pe on pe.ssn=pa.ssn where pa.ssn={};".format(
-            patientSsn
-        )
+        query = """select * from patient natural join person where ssn=%s;"""
         mycursor = db.cursor()
-        mycursor.execute(query)
+        mycursor.execute(query, (patientSsn,))
         patient = mycursor.fetchall()[0]
 
         self.first.delete(0, ctk.END)
-        self.first.insert(string=patient[0], index=0)
+        self.first.insert(string=patient[4], index=0)
         self.last.delete(0, ctk.END)
-        self.last.insert(string=patient[1], index=0)
+        self.last.insert(string=patient[3], index=0)
         self.address.delete(0, ctk.END)
         self.address.insert(string=patient[2], index=0)
         self.birthdate.delete(0, ctk.END)
-        self.birthdate.insert(string=patient[3], index=0)
+        self.birthdate.insert(string=patient[5], index=0)
         rooms = getUnoccupiedPatientRooms()
-        rooms.append(str(patient[4]))
+        rooms.append(str(patient[1]))
         self.roomNum.configure(values=rooms)  # append current patient room number
 
-    def getPatients(self) -> list[str]:
-        query = "select pa.ssn,first_name,last_name from patient as pa join person as pe on pa.ssn=pe.ssn;"
-        mycursor = db.cursor()
-        mycursor.execute(query)
-        result = mycursor.fetchall()
-        patients = [
-            str(patient[0]) + " " + str(patient[1]) + " " + str(patient[2])
-            for patient in result
-        ]
-        if len(patients) == 0:
-            return [""]
-        return patients
-
     def tkraise(self):
-        self.patients.configure(values=self.getPatients())
+        self.patients.configure(values=getPatients())
         self.first.delete(0, ctk.END)
         self.first.configure(placeholder_text="First name")
         self.last.delete(0, ctk.END)
@@ -334,7 +345,6 @@ class PatientBySSN(ctk.CTkFrame):
         back = ctk.CTkButton(
             self, text="Back", command=lambda: self.controller.show_frame("PatientOpts")
         )
-        self.entries = [self.ssn]
         label.pack(side="top", fill="x", pady=10)
         self.ssn.pack()
         self.searchBtn.pack()
@@ -342,17 +352,18 @@ class PatientBySSN(ctk.CTkFrame):
         self.results_box.pack()
 
     def search(self):
-        query = "select first_name,last_name,pa.ssn,address,birthdate,room_num from patient as pa join person as pe on pa.ssn=pe.ssn where pa.ssn={}"
+        # query = """select first_name,last_name,pa.ssn,address,birthdate,room_num from patient as pa join person as pe on pa.ssn=pe.ssn where pa.ssn=%s"""
+        query="""select * from patient natural join person where ssn=%s;"""
         mycursor = db.cursor()
-        mycursor.execute(query.format(self.ssn.get()))
+        mycursor.execute(query, (self.ssn.get(),))
         people = mycursor.fetchall()
         labelText = ""
         if len(people) == 0:
             labelText = "There is no patient with ssn {}".format(self.ssn.get())
         else:
             person = people[0]
-            labelText = "Name: {} {}\nSSN: {}\nBirthdate: {}\nAddress: {}\nRoom Number:{}\n\n".format(
-                person[0], person[1], person[2], person[3], person[4], person[5]
+            labelText = "Name: {} {}\nSSN: {}\nAddress: {}\nBirthdate: {}\nRoom Number:{}\n\n".format(
+                person[4], person[3], person[0], person[2], person[5], person[1]
             )
         self.results_box.configure(state=ctk.NORMAL)
         self.results_box.delete("0.0", "end")
