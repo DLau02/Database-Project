8d5e6c24d56199ef30629715bdbc0d1623410b07
mohammadfarazk <107908099+mohammadfarazk@users.noreply.github.com>
2023-08-02 15:06:17 -0500

    Update DoctorOpts.py

--- gui/DoctorOpts.py
+++ gui/DoctorOpts.py
@@ -1,16 +1,18 @@
 import customtkinter as ctk
 from dbConfig import db
 
-def getUnoccupiedDoctorRooms() -> list[str]:
+
+'''def getUnoccupiedDoctorRooms() -> list[str]:
     # get unoccupied doctor room numbers
-    query = "SELECT room_num FROM room WHERE room_type='Doctor Office' AND room_num NOT IN (SELECT room_num FROM doctor);"
+    query = "SELECT Room_Num FROM room WHERE room_type='Doctor Office' AND Room_Num NOT IN (SELECT Room_Num FROM doctor);"
     mycursor = db.cursor()
     mycursor.execute(query)
     unoccupied_rooms = mycursor.fetchall()
     rooms = [str(room[0]) for room in unoccupied_rooms]
     if len(rooms) == 0:
-        return [""]
-    return rooms
+        return ["No offices available"]
+    return rooms'''
+
 
 class DoctorOpts(ctk.CTkFrame):
     def __init__(self, parent, controller):
@@ -121,13 +123,15 @@ class AddDoctor(ctk.CTkFrame):
         self.first = ctk.CTkEntry(self, placeholder_text="first name", width=200)
         self.last = ctk.CTkEntry(self, placeholder_text="last name", width=200)
         self.ssn = ctk.CTkEntry(self, placeholder_text="SSN", width=200)
-        self.specialty = ctk.CTkEntry(
-            self, placeholder_text="Specialty", width=200
-        )
+        self.date = ctk.CTkEntry(self, placeholder_text="date of birth", width=200)
+        self.address = ctk.CTkEntry(self, placeholder_text="address", width=200)
+        self.specialty = ctk.CTkEntry(self, placeholder_text="specialty", width=200)
+        self.department = ctk.CTkEntry(self, placeholder_text="department", width=200)
 
         # get unoccupied doctor room numbers
-        self.roomNum = ctk.CTkOptionMenu(self, values=getUnoccupiedDoctorRooms())
-        self.room_num_label = ctk.CTkLabel(self, text="Room number")
+        #optionbox_var = ctk.StringVal(value="No room Available")
+        #self.roomNum = ctk.CTkOptionMenu(self, values=getUnoccupiedDoctorRooms(), variable=optionbox_var, command=getUnoccupiedDoctorRooms())
+        #self.room_num_label = ctk.CTkLabel(self, text="Room number")
 
         add = ctk.CTkButton(
             self,
@@ -142,9 +146,12 @@ class AddDoctor(ctk.CTkFrame):
         self.first.pack()
         self.last.pack()
         self.ssn.pack()
+        self.address.pack()
+        self.date.pack()
         self.specialty.pack()
-        self.room_num_label.pack()
-        self.roomNum.pack(pady=5)
+        self.department.pack()
+        #self.room_num_label.pack()
+        #self.roomNum.pack(pady=5)
         add.pack()
         back.pack()
         self.resultsBox.pack()
@@ -156,43 +163,67 @@ class AddDoctor(ctk.CTkFrame):
         self.last.configure(placeholder_text="Last name")
         self.ssn.delete(0, ctk.END)
         self.ssn.configure(placeholder_text="SSN")
-        self.specialty.delete(0, ctk.END)
-        self.specialty.configure(placeholder_text="Specialty")
+        #self.specialty.delete(0, ctk.END)
+        #self.specialty.configure(placeholder_text="Specialty")
+        self.address.delete(0, ctk.END)
+        self.address.configure(placeholder_text = "Address")
+        self.date.delete(0, ctk.END)
+        self.date.configure(placeholder_text = "Date of birth")
         self.resultsBox.configure(state=ctk.NORMAL)
         self.resultsBox.delete("0.0", "end")
         self.resultsBox.configure(state=ctk.DISABLED)
         super().tkraise()
 
     def add(self):
-        person_query = "INSERT INTO person VALUES ({}, '{}', '{}', NULL, NULL);"
-        doctor_query = "INSERT INTO doctor VALUES ({}, '{}', '{}');"
-        mycursor = db.cursor()
-        try:
-            mycursor.execute(
-                person_query.format(
+        person_query = "INSERT INTO person VALUES (%s, %s, %s, %s, STR_TO_DATE(%s, '%Y-%m-%d'));"
+        doctor_query = "INSERT INTO doctor VALUES (%s);"
+        query2 = "Insert Into employee Values (%s, %s, %s)"
+        queryVal2 = (self.ssn.get(), self.specialty.get(), self.department.get())
+        queryVal=(
                     self.ssn.get(),
+                    self.address.get(),
                     self.last.get(),
                     self.first.get(),
-                )
-            )
+                    self.date.get(),
+        )
+        queryValue=(
+            self.ssn.get(),
+        )
+
+        mycursor = db.cursor()
+
+        try:
+            
             mycursor.execute(
-                doctor_query.format(
-                    self.ssn.get(), self.specialty.get()
-                )
+                person_query, queryVal
             )
             db.commit()
+            mycursor.execute(query2, queryVal2)
+            db.commit()
+            mycursor.execute(doctor_query, queryValue)
+            db.commit()
+
             self.resultsBox.configure(state=ctk.NORMAL)
             self.resultsBox.delete("0.0", "end")
             self.resultsBox.insert(
                 "0.0",
-                "Added Dr. {} {} to the database".format(self.first.get(), self.last.get()),
+                "Added Dr. {} {} to the database".format(
+                    self.first.get(), self.last.get()
+                ),
             )
             self.resultsBox.configure(state=ctk.DISABLED)
-            self.roomNum.configure(
+        except Exception as e:
+            db.rollback()  # Rollback the transaction in case of an error
+            self.resultsBox.configure(state=ctk.NORMAL)
+            self.resultsBox.delete("0.0", "end")
+            label_text = "Error occurred: {}".format(str(e))
+            self.resultsBox.insert("0.0", label_text)
+            self.resultsBox.configure(state=ctk.DISABLED)
+            '''self.roomNum.configure(
                 values=getUnoccupiedDoctorRooms(),
-            )
-            self.roomNum.set(self.roomNum._values[0])
-        except:
+            )'''
+            #self.roomNum.set(self.roomNum._values[0])
+        '''except:
             self.resultsBox.configure(state=ctk.NORMAL)
             self.resultsBox.delete("0.0", "end")
             self.resultsBox.insert(
@@ -201,8 +232,9 @@ class AddDoctor(ctk.CTkFrame):
                     self.first.get(), self.last.get()
                 ),
             )
-            self.resultsBox.configure(state=ctk.DISABLED)
 
+            self.resultsBox.configure(state=ctk.DISABLED)'''
+        
 
 class ChangeDoctor(ctk.CTkFrame):
     def __init__(self, parent, controller):
@@ -220,8 +252,8 @@ class ChangeDoctor(ctk.CTkFrame):
         self.first = ctk.CTkEntry(self, placeholder_text="First name")
         self.last = ctk.CTkEntry(self, placeholder_text="Last name")
         self.specialty = ctk.CTkEntry(self, placeholder_text="Specialty")
-        self.roomNumLabel = ctk.CTkLabel(self, text="Room number")
-        self.roomNum = ctk.CTkOptionMenu(self, values=getUnoccupiedDoctorRooms())
+        # self.roomNumLabel = ctk.CTkLabel(self, text="Room number")
+        # self.roomNum = ctk.CTkOptionMenu(self, values=getUnoccupiedDoctorRooms())
         self.changeBtn = ctk.CTkButton(self, text="Change", command=self.change)
 
         self.back = ctk.CTkButton(
@@ -237,25 +269,32 @@ class ChangeDoctor(ctk.CTkFrame):
         self.first.pack()
         self.last.pack()
         self.specialty.pack()
-        self.roomNumLabel.pack()
-        self.roomNum.pack()
+        # self.roomNumLabel.pack()
+        # self.roomNum.pack()
         self.changeBtn.pack()
         self.resultsBox.pack()
         self.back.pack(pady=10)
 
     def change(self):
         try:
-            personQuery = "UPDATE person SET first_name='{}', last_name='{}' WHERE ssn={};".format(
-                self.first.get(),
-                self.last.get(),
-                self.doctors.get().split(" ")[0],  # ssn
-            )
-            doctorQuery = "UPDATE doctor SET specialty='{}', room_num={} WHERE ssn={}".format(
-                self.specialty.get(), self.roomNum.get(), self.doctors.get().split(" ")[0]
-            )
+            # personQuery = "UPDATE person SET first_name='{}', last_name='{}' WHERE ssn={};".format(
+            #     self.first.get(),
+            #     self.last.get(),
+            #     self.doctors.get().split(" ")[0],  # ssn
+            # )
+            # doctorQuery = "UPDATE employee SET specialty='{}' WHERE ssn={}".format(
+            #     self.specialty.get(),
+            #     # self.roomNum.get(),
+            #     self.doctors.get().split(" ")[0],  # ssn
+            # )
+            personQuery = "UPDATE person SET first_name=%s, last_name=%s WHERE ssn=%s;"
+            doctorQuery = "UPDATE employee SET specialty=%s WHERE ssn=%s"
             mycursor = db.cursor()
-            mycursor.execute(personQuery)
-            mycursor.execute(doctorQuery)
+            mycursor.execute(
+                personQuery,
+                (self.first.get(), self.last.get(), self.doctors.get().split(" ")[0]),
+            )
+            mycursor.execute(doctorQuery, (self.specialty.get(),self.doctors.get().split(" ")[0]))
             db.commit()
             labelText = "Dr. {} information was changed".format(self.doctors.get())
             self.resultsBox.configure(state=ctk.NORMAL)
@@ -271,22 +310,23 @@ class ChangeDoctor(ctk.CTkFrame):
 
     def populateEntries(self):
         doctorSsn = self.doctors.get().split(" ")[0]
-        query = "SELECT first_name, last_name, specialty, room_num FROM doctor AS d JOIN person AS pe ON pe.ssn = d.ssn WHERE d.ssn={};".format(
-            doctorSsn
-        )
+        # query = "SELECT first_name, last_name, specialty, room_num FROM doctor AS d JOIN person AS pe ON pe.ssn = d.ssn WHERE d.ssn={};".format(
+        #     doctorSsn
+        # )
+        query = "select * from doctor natural join person natural join employee where ssn=%s"
         mycursor = db.cursor()
-        mycursor.execute(query)
+        mycursor.execute(query, (doctorSsn,))
         doctor = mycursor.fetchall()[0]
 
         self.first.delete(0, ctk.END)
-        self.first.insert(string=doctor[0], index=0)
+        self.first.insert(string=doctor[3], index=0)
         self.last.delete(0, ctk.END)
-        self.last.insert(string=doctor[1], index=0)
+        self.last.insert(string=doctor[2], index=0)
         self.specialty.delete(0, ctk.END)
-        self.specialty.insert(string=doctor[2], index=0)
-        rooms = getUnoccupiedDoctorRooms()
-        rooms.append(str(doctor[3]))
-        self.roomNum.configure(values=rooms)  # append current doctor room number
+        self.specialty.insert(string=doctor[5], index=0)
+        # rooms = getUnoccupiedDoctorRooms()
+        # rooms.append(str(doctor[3]))
+        # self.roomNum.configure(values=rooms)  # append current doctor room number
 
     def getDoctors(self) -> list[str]:
         query = "SELECT d.ssn, first_name, last_name FROM doctor AS d JOIN person AS pe ON d.ssn = pe.ssn;"
